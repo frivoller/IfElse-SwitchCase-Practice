6abbc21e37cb993e6574475cdbda859899deaa82
irl <irl@DESKTOP-DIAN65C>
2024-10-03 11:37:57 +0300

    Add project files.

---
+++ ifelse.cs
@@ -0,0 +1,38 @@
+﻿//if else çözümü, çözüm olarak switchcase tercih edilmeli çünkü kod daha organize ve okunabilir.
+using System;
+
+class ifelse
+{
+    static int ManavFiyatiIf(string meyve)
+    {
+        if (meyve == "Elma")
+        {
+            return 5;
+        }
+        else if (meyve == "Armut")
+        {
+            return 3;
+        }
+        else if (meyve == "Çilek")
+        {
+            return 2;
+        }
+        else if (meyve == "Muz")
+        {
+            return 3;
+        }
+        else
+        {
+            return 4; // Diğer bütün meyveler
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ManavFiyatiIf("Elma"));   // 5
+        Console.WriteLine(ManavFiyatiIf("Armut"));  // 3
+        Console.WriteLine(ManavFiyatiIf("Çilek"));  // 2
+        Console.WriteLine(ManavFiyatiIf("Muz"));    // 3
+        Console.WriteLine(ManavFiyatiIf("Kavun"));  // 4
+    }
+}
---
+++ switch.cs
@@ -0,0 +1,30 @@
+﻿//switchcase çözümü. çözüm olarak switchcase tercih edilmeli çünkü kod daha organize ve okunabilir.
+
+class Program
+{
+    static int ManavFiyatiSwitch(string meyve)
+    {
+        switch (meyve)
+        {
+            case "Elma":
+                return 5;
+            case "Armut":
+                return 3;
+            case "Çilek":
+                return 2;
+            case "Muz":
+                return 3;
+            default:
+                return 4; // Diğer bütün meyveler
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ManavFiyatiSwitch("Elma"));   // 5
+        Console.WriteLine(ManavFiyatiSwitch("Armut"));  // 3
+        Console.WriteLine(ManavFiyatiSwitch("Çilek"));  // 2
+        Console.WriteLine(ManavFiyatiSwitch("Muz"));    // 3
+        Console.WriteLine(ManavFiyatiSwitch("Kavun"));  // 4
+    }
+}
